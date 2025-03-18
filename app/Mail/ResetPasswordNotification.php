40682de74837bd4aff08fef232c468730a411aec
MomarNdiayeSy <momarndiayesy10@gmail.com>
2025-03-18 23:49:30 +0000

    Troisieme changement

---
+++ app/Mail/ResetPasswordNotification.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class ResetPasswordNotification extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $url;
+
+    public function __construct($url)
+    {
+        $this->url = $url;
+    }
+
+    public function build()
+    {
+        return $this->subject('Réinitialisation de votre mot de passe')
+            ->view('emails.reset_password')
+            ->with(['url' => $this->url]);
+    }
+}
